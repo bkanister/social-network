5bb3b4ef4be1f95d6d854836b33304b06be98e86
bkanister <b.kanister@gmail.com>
2020-06-04 13:47:29 +0300

    added auto upload for post images

--- src/components/ImageHandler/ImageInput.js
+++ src/components/ImageHandler/ImageInput.js
@@ -3,9 +3,8 @@ import React, {useState} from 'react'
 const ImageInput = props => {
     return (
         <div>
-            <form onSubmit={(e) => props.handleFireBaseUpload(e)}>
-                <input type="file" onChange={(e) => props.handleImageAsFile(e)}/>
-                <button type="submit">Upload</button>
+            <form>
+                <input type="file" onChange={(e) => props.downloadImage(e)}/>
                 {props.postImage
                     ? <img style={{width: '100%'}} src={props.postImage} alt="image tag"/>
                     : null}
--- src/components/ImageHandler/ImageInputContainer.js
+++ src/components/ImageHandler/ImageInputContainer.js
@@ -4,54 +4,35 @@ import ImageInput from "./ImageInput";
 
 const ImageInputContainer = props => {
 
-    const allInputs = {imgUrl: ''}
-    const [imageAsFile, setImageAsFile] = useState('')
-    const [imageAsUrl, setImageAsUrl] = useState(allInputs)
-
-    console.log(imageAsFile)
-    const handleImageAsFile = (e) => {
-        const image = e.target.files[0]
-        setImageAsFile(imageFile => (image))
+    const downloadImage = (e) => {
+        return new Promise((resolve, reject) => {
+            const image = e.target.files[0]
+            if (image) {
+                resolve(image)
+            } else {
+                let error = new Error('something went wrong')
+                reject(error)
+            }
+        }).then((image) => handleFireBaseUpload(image))
     }
 
-    const handleFireBaseUpload = e => {
-        e.preventDefault()
-        console.log('start of upload')
-        // async magic goes here...
-        if(imageAsFile === '') {
-            console.error(`not an image, the image file is a ${typeof(imageAsFile)}`)
-        }
-        const uploadTask = storage.ref(`/images/${imageAsFile.name}`).put(imageAsFile)
-        //initiates the firebase side uploading
+    const handleFireBaseUpload = (image) => {
+        const uploadTask = storage.ref(`/images/${image.name}`).put(image)
         uploadTask.on('state_changed',
             (snapShot) => {
-                //takes a snap shot of the process as it is happening
                 console.log(snapShot)
             }, (err) => {
-                //catches the errors
                 console.log(err)
             }, () => {
-                // gets the functions from storage refences the image storage in firebase by the children
-                // gets the download url then sets the image from firebase as the value for the imgUrl key:
-                storage.ref('images').child(imageAsFile.name).getDownloadURL()
+                storage.ref('images').child(image.name).getDownloadURL()
                     .then(fireBaseUrl => {
-                            setImageAsUrl(prevObject => {
-                                    props.dispatch({type: 'ADD_PHOTO', payload: fireBaseUrl})
-                                    return {
-                                        ...prevObject,
-                                        imgUrl: fireBaseUrl
-                                    }
-                                }
-                            )
-                        }
-                    )
-            }
-        )
+                        props.dispatch({type: 'ADD_PHOTO', payload: fireBaseUrl})
+                    })
+        })
     }
 
     return <ImageInput
-                handleImageAsFile={handleImageAsFile}
-                handleFireBaseUpload={handleFireBaseUpload}
+                downloadImage={downloadImage}
                 postImage={props.postImage}
             />
 }
